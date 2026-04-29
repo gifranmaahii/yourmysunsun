442c3b8f28350a490dbf213271d6f1a1ba376053
notwhyy100 <notwhyy100@example.com>
2026-04-29 15:55:39 +0700

    refactor: simplify bot-robby config for standard login

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,10 @@ module.exports = {
     {
       name: 'bot-robby',
       script: 'index.js',
-      args: '--session=robby_v2',
       cwd: __dirname,
       autorestart: true,
       max_restarts: 50,
-      restart_delay: 5000,
-      env: {
-        PAIRING_NUMBER: "6282312867226"
-      }
+      restart_delay: 5000
     },
     {
       name: 'bot-member-6289672768769',
