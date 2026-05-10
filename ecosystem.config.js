e2eb2cb08cc5152502f0fc75ffe5821192a634b4
notwhyy100 <notwhyy100@example.com>
2026-05-10 09:39:49 +0700

    Fix Telegram 409 conflict by running single instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,14 +10,6 @@ module.exports = {
       env: {
         PAIRING_NUMBER: "6282312867226"
       }
-    },
-    {
-      name: 'bot-telegram-panel',
-      script: 'telegramControl.js',
-      cwd: __dirname,
-      autorestart: true,
-      max_restarts: 50,
-      restart_delay: 5000
     }
   ]
 };
